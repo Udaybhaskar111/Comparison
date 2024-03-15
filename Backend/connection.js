1192fbfe941c3b08dc70e71c0b7169279914c647
unknown <vinay.dudi@divami.com>
2024-03-15 09:52:26 +0530

    folder structure

---
+++ Backend/connection.js
@@ -0,0 +1,49 @@
+import * as fs from "fs";
+import { Sequelize } from "sequelize";
+export const connection =async(req,res)=>{
+      const {body}=req;
+      const {TabelName,DBtype,passwd,user_id,DBname}=body;
+      const sequelize = new Sequelize(DBname, user_id, passwd
+    , {
+      host: 'localhost',
+      dialect: DBtype,
+    });
+     sequelize.authenticate().then(() => {
+        console.log('Database Connected');  
+      //  sequelize.query(`SELECT * FROM ${TabelName}`, { type: sequelize.QueryTypes.SELECT })
+      //   // sequelize.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = hr`)
+      //   .then(results => {
+      //     const jsonData = JSON.stringify(results, null, 2);
+      //       fs.writeFile('data.json', jsonData, (err) => {
+      //           if (err) {
+      //             console.error("Error writing JSON file:", err);
+      //           } else {
+      //             console.log("JSON file has been saved.");
+      //           }
+      //         });
+      //     res.json({"results":results});
+      //   })
+      //   .catch(error => {
+      //     console.error('Error executing query:', error);
+      //   });
+      // })
+      sequelize
+  .query(
+    'SELECT column_name, data_type,is_nullable, column_default FROM information_schema.columns WHERE table_name = :tableName',
+    {
+      replacements: { tableName: 'samp' },
+      type: sequelize.QueryTypes.SELECT,
+    }
+  )
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((error) => {
+    console.error('Error executing query:', error);
+  });
+})
+      .catch((err) => {
+        console.error('Unable to connect to the DataBase:', err);
+      });
+    }
+
---
+++ Backend/sendheader.js
@@ -0,0 +1,7 @@
+import datafile from "./datafile.json" assert { type: "json" };
+import datadb from "./data.json" assert { type: "json" };
+export const sendheader = async (req, res) => {
+  const obj1 = Object.keys(datafile[0]);
+  const obj2 = Object.keys(datadb[0]);
+  res.send({ file: obj1, database: obj2 });
+};
