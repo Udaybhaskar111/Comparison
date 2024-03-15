27b8e18c6d4ecb690e8924ad1f9ba66138a9fe4d
vinay-dudi-divami <144336446+vinay-dudi-divami@users.noreply.github.com>
2024-03-15 10:02:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,7 @@ import { sqlite } from "./sqliteConnection.js";
 import { initialcompare } from "./initialcompare.js";
 import { oracle } from "./oracle.js";
 // import {all} from './all.js';
+// import {opt} from './optimisedCompare.js'
 import { sendheader } from "./sendheader.js";
 const app = express();
 app.use(
@@ -18,6 +19,7 @@ app.use(
 const upload = multer({ dest: "uploads/" });
 app.use("/file", upload.single("file"), send);
 app.use(bodyParser.json());
+// app.use('/opt',opt)
 app.use("/info", sendheader);
 app.use("/initialCompare", initialcompare);
 app.use("/sqlite", sqlite);
