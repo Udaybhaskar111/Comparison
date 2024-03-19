898b3f5c82255b1ad2607605328d017cbe80a330
UdaybhaskarTalari <20a91a1252@aec.edu.in>
2024-03-19 10:57:52 +0530

    latest code

---
+++ Backend/controllers/compare.js
@@ -0,0 +1,162 @@
+// import datafile from "../constants/datafile.json" assert { type: "json" };
+// import datadb from "../constants/data.json" assert { type: "json" };
+// import primaryKey from '../constants/primarykey.json' assert {type:"json"};
+// import { json2csv } from "json-2-csv";
+// // import {optimise} from "./optimise.js"
+// import * as fs from "fs";
+// import { Router } from "express";
+// import { assert } from "console";
+// import { type } from "os";
+// let mismatch=[]
+
+// export const compare = async (req, res) => {
+//  let dataDB2={};
+// //  let {body}=req;
+//   const key = primaryKey[0].primarykey; 
+//   for (const record of datadb) {
+//     const key = primaryKey[0].primarykey; 
+//     dataDB2[record[key]] = record;
+// }
+//   for(let i = 0; i < datafile.length; i++)
+//   {
+//     const dataFileprimaryValue=datafile[i][key];
+//     const dbRecord= dataDB2[dataFileprimaryValue];
+//     let dataRecord=Object.values(datafile[i]);
+//     if(dbRecord){
+//     const dbRecordValues=Object.values(dbRecord)
+//     for(let j=0;j<dataRecord.length;j++){
+//       let type = typeof dbRecordValues[i];
+//       try{
+//         switch (type) {
+//           case "number":
+//             dataRecord[i] = Number(dataRecord[i]);
+//             break;
+//           case "string":
+//             dataRecord[i] = String(dataRecord[i]);
+//             break;
+//           case "boolean":
+//             dataRecord[i] = Boolean(dataRecord[i]);
+//             break;
+//           case "date":
+//             dataRecord[i] = Date(dataRecord[i]);
+//           default:
+//             dataRecord[i]=dataRecord[i];
+//         }
+//       }
+//       catch(error){
+//         console.log("hello");
+//       }
+//       if((dbRecordValues[j]==dataRecord[j]) || (dbRecordValues[j] == null && dataRecord[j] == null) || (dbRecordValues[j] == "" && dataRecord[j] == null)){
+//         // if (type=="string"){
+//         //   if(dataRecord[i].toLowerCase() !== dbRecordValues[i].toLowerCase()) {
+//         //     mismatch.push(dataRecord)
+//         //   }
+//         // } 
+//         continue;
+//       }
+//       else{
+//         mismatch.push(dataRecord)
+//         break;
+//       }
+//     }
+//   }
+//   else{
+//     mismatch.push(dataRecord)
+//   }
+// }
+//   console.log(mismatch,"mismatches")
+// };
+
+// export const sendfile = async (req, res) => {
+//   console.log(mismatch, "as lost ");
+//   const csv = json2csv(mismatch);
+//   console.log(csv, "format");
+//   fs.writeFile("missingdata.csv", csv, (error) => {
+//     if (error) {
+//       console.log(error);
+//       res.send("try after some time");
+//     } else {
+//       const filePath = "./missingData.csv";
+//       const fileName = "data.csv";
+//       res.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+//       res.setHeader("Content-Type", "text/csv");
+//       const fileStream = fs.createReadStream(filePath);
+//       fileStream.pipe(res);
+//     }
+//   });
+// };
+
+import datafile from "../constants/datafile.json" assert { type: "json" };
+import datadb from "../constants/data.json" assert { type: "json" };
+import primaryKey from '../constants/primarykey.json' assert {type:"json"};
+import { json2csv } from "json-2-csv";
+import * as fs from "fs";
+let mismatch = [];
+export const compare = async (req, res) => {
+  mismatch=[]
+  const key = primaryKey[0].primarykey;
+  let dataDB2 = {};
+  for (const record of datadb) {
+    dataDB2[record[key]] = record;
+  }
+
+  for (let i = 0; i < datafile.length; i++) {
+    const dataFileprimaryValue = datafile[i][key];
+    const dbRecord = dataDB2[dataFileprimaryValue];
+    if (dbRecord) {
+      const dbRecordValues = Object.values(dbRecord);
+      let dataRecord = Object.values(datafile[i]);
+      for (let j = 0; j < dataRecord.length; j++) {
+        let type = typeof dbRecordValues[j];
+        try {
+          switch (type) {
+            case "number":
+              dataRecord[j] = Number(dataRecord[j]);
+              break;
+            case "string":
+              dataRecord[j] = String(dataRecord[j]);
+              break;
+            case "boolean":
+              dataRecord[j] = Boolean(dataRecord[j]);
+              break;
+            case "date":
+              dataRecord[j] = new Date(dataRecord[j]);
+              break;
+            default:
+              dataRecord[j] = dataRecord[j];
+          }
+        } catch (error) {
+          console.log("Error converting data:", error);
+        }
+        if ((dbRecordValues[j] === dataRecord[j]) || (dbRecordValues[j] == null && dataRecord[j] == null) || (dbRecordValues[j] == "" && dataRecord[j] == null)) {
+          continue;
+        } else {
+          mismatch.push(datafile[i]);
+          break;
+        }
+      }
+    } else {
+      mismatch.push(datafile[i]);
+    }
+  }
+  res.send({ Filedata: datafile, LostData: mismatch, message: "" });
+  console.log(mismatch, "mismatches");
+};
+
+export const sendfile = async (req, res) => {
+   const csv = json2csv(mismatch);
+  console.log(csv, "format");
+  fs.writeFile("./missingdata.csv", csv, (error) => {
+    if (error) {
+      console.log(error);
+      res.send("try after some time");
+    } else {
+      const filePath = "./missingdata.csv";
+      const fileName = "missingdata.csv";
+      res.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+      res.setHeader("Content-Type", "text/csv");
+      const fileStream = fs.createReadStream(filePath);
+      fileStream.pipe(res);
+    }
+  });
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import multer from "multer";
-import route from "./routes.js";
+import route from "./routes/routes.js";
 import cors from "cors";
-import { send } from "./excel.js";
+import { send } from "./controllers/fileupload/excel.js";
 import { sqlite } from "./sqliteConnection.js";
-import { initialcompare } from "./initialcompare.js";
-import { oracle } from "./oracle.js";
+// import { sequelize } from "./config/sequlize.js";
+import { initialcompare } from "./controllers/initialcompare.js";
+import { oracle } from "./config/oracle.js";
 // import {all} from './all.js';
 // import {opt} from './optimisedCompare.js'
-import { sendheader } from "./sendheader.js";
+import { sendheader } from "./controllers/fileupload/sendheader.js";
+// import { optimise } from "./optimise.js";
 const app = express();
 app.use(
   cors({
@@ -22,8 +24,9 @@ app.use(bodyParser.json());
 // app.use('/opt',opt)
 app.use("/info", sendheader);
 app.use("/initialCompare", initialcompare);
-app.use("/sqlite", sqlite);
-app.use("/oracle", oracle);
-
+// app.use("/sqlite", sqlite);
+// app.use("/oracle", oracle);
+// app.post('/',optimise);
+// app.use('/',sequelize)
 app.use("/", route);
 app.listen(8082, () => console.log("connected"));
